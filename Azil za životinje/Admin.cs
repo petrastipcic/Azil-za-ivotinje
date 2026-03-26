db195eac96672c45aa447e55ad0e99281b89747d
petrastipcic <petra.stipcic@skole.hr>
2026-03-26 13:10:06 +0100

    Promjene

--- Azil za životinje/Admin.cs
+++ Azil za životinje/Admin.cs
@@ -9,26 +9,6 @@ namespace Azil_za_životinje
 {
     public static class Admin
     {
-        public static void SpremiPodatke(string linija)
-        {
-            StreamWriter sw = new StreamWriter("Podaci.txt", true);
-            sw.WriteLine(linija);
-            sw.Close();
-
-        }
-        public static List<string> UcitajPodatke()
-        {
-            List<string> lista = new List<string>();
-            StreamReader sr = new StreamReader("Podaci.txt");
-            string linija=sr.ReadLine();
-            while ((linija!=null))
-            {
-                lista.Add(linija);
-                linija = sr.ReadLine();
-            }
-
-            sr.Close();
-            return lista;
-        }
+       
     }
 }
