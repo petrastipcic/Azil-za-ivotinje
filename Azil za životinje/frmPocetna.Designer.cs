8492e3cea63f508fe7d200c60661ccd4cc7fef6b
petrastipcic <petra.stipcic@skole.hr>
2026-03-29 19:19:42 +0200

    Promjene

--- Azil za životinje/frmPocetna.Designer.cs
+++ Azil za životinje/frmPocetna.Designer.cs
@@ -38,71 +38,78 @@
             // 
             // btnUnosZivotinje
             // 
-            this.btnUnosZivotinje.Location = new System.Drawing.Point(12, 12);
+            this.btnUnosZivotinje.Location = new System.Drawing.Point(16, 15);
+            this.btnUnosZivotinje.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnUnosZivotinje.Name = "btnUnosZivotinje";
-            this.btnUnosZivotinje.Size = new System.Drawing.Size(90, 23);
+            this.btnUnosZivotinje.Size = new System.Drawing.Size(120, 28);
             this.btnUnosZivotinje.TabIndex = 0;
             this.btnUnosZivotinje.Text = "Unos životinje";
             this.btnUnosZivotinje.UseVisualStyleBackColor = true;
             // 
             // btnPregled
             // 
-            this.btnPregled.Location = new System.Drawing.Point(12, 41);
+            this.btnPregled.Location = new System.Drawing.Point(16, 50);
+            this.btnPregled.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnPregled.Name = "btnPregled";
-            this.btnPregled.Size = new System.Drawing.Size(90, 23);
+            this.btnPregled.Size = new System.Drawing.Size(120, 28);
             this.btnPregled.TabIndex = 1;
             this.btnPregled.Text = "Pregled";
             this.btnPregled.UseVisualStyleBackColor = true;
             // 
             // btnFiltriranje
             // 
-            this.btnFiltriranje.Location = new System.Drawing.Point(12, 70);
+            this.btnFiltriranje.Location = new System.Drawing.Point(16, 86);
+            this.btnFiltriranje.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnFiltriranje.Name = "btnFiltriranje";
-            this.btnFiltriranje.Size = new System.Drawing.Size(90, 23);
+            this.btnFiltriranje.Size = new System.Drawing.Size(120, 28);
             this.btnFiltriranje.TabIndex = 2;
             this.btnFiltriranje.Text = "Filtriranje";
             this.btnFiltriranje.UseVisualStyleBackColor = true;
             // 
             // btnUdomljavanje
             // 
-            this.btnUdomljavanje.Location = new System.Drawing.Point(12, 99);
+            this.btnUdomljavanje.Location = new System.Drawing.Point(16, 122);
+            this.btnUdomljavanje.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnUdomljavanje.Name = "btnUdomljavanje";
-            this.btnUdomljavanje.Size = new System.Drawing.Size(90, 23);
+            this.btnUdomljavanje.Size = new System.Drawing.Size(120, 28);
             this.btnUdomljavanje.TabIndex = 3;
             this.btnUdomljavanje.Text = "Udomljavanje";
             this.btnUdomljavanje.UseVisualStyleBackColor = true;
             // 
             // btnStatistika
             // 
-            this.btnStatistika.Location = new System.Drawing.Point(12, 128);
+            this.btnStatistika.Location = new System.Drawing.Point(16, 158);
+            this.btnStatistika.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnStatistika.Name = "btnStatistika";
-            this.btnStatistika.Size = new System.Drawing.Size(90, 23);
+            this.btnStatistika.Size = new System.Drawing.Size(120, 28);
             this.btnStatistika.TabIndex = 4;
             this.btnStatistika.Text = "Statistika";
             this.btnStatistika.UseVisualStyleBackColor = true;
             // 
             // btnIzlaz
             // 
-            this.btnIzlaz.Location = new System.Drawing.Point(12, 157);
+            this.btnIzlaz.Location = new System.Drawing.Point(16, 193);
+            this.btnIzlaz.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnIzlaz.Name = "btnIzlaz";
-            this.btnIzlaz.Size = new System.Drawing.Size(90, 23);
+            this.btnIzlaz.Size = new System.Drawing.Size(120, 28);
             this.btnIzlaz.TabIndex = 5;
             this.btnIzlaz.Text = "Izlaz";
             this.btnIzlaz.UseVisualStyleBackColor = true;
             // 
             // frmPocetna
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.btnIzlaz);
             this.Controls.Add(this.btnStatistika);
             this.Controls.Add(this.btnUdomljavanje);
             this.Controls.Add(this.btnFiltriranje);
             this.Controls.Add(this.btnPregled);
             this.Controls.Add(this.btnUnosZivotinje);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmPocetna";
-            this.Text = "Form1";
+            this.Text = "Početna";
             this.ResumeLayout(false);
 
         }
