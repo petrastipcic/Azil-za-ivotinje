1048377a3f0bc67ca9ed8cf6cdeee5d829aa9f67
petrastipcic <petra.stipcic@skole.hr>
2026-03-29 14:12:42 +0200

    Promjene

--- Azil za životinje/frmUnosZivotinje.Designer.cs
+++ Azil za životinje/frmUnosZivotinje.Designer.cs
@@ -45,77 +45,90 @@
             this.cbKastriran = new System.Windows.Forms.CheckBox();
             this.lblNapomena = new System.Windows.Forms.Label();
             this.txtNapomena = new System.Windows.Forms.TextBox();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.lblOdabir = new System.Windows.Forms.Label();
             this.pbOdabir = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.btnOdaberiSliku = new System.Windows.Forms.Button();
+            this.cbStatus = new System.Windows.Forms.ComboBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.nudDob)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbOdabir)).BeginInit();
             this.SuspendLayout();
             // 
             // txtIme
             // 
-            this.txtIme.Location = new System.Drawing.Point(66, 16);
+            this.txtIme.Location = new System.Drawing.Point(88, 20);
+            this.txtIme.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtIme.Name = "txtIme";
-            this.txtIme.Size = new System.Drawing.Size(149, 20);
+            this.txtIme.Size = new System.Drawing.Size(197, 22);
             this.txtIme.TabIndex = 0;
             // 
             // lblIme
             // 
             this.lblIme.AutoSize = true;
-            this.lblIme.Location = new System.Drawing.Point(12, 19);
+            this.lblIme.Location = new System.Drawing.Point(16, 23);
+            this.lblIme.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblIme.Name = "lblIme";
-            this.lblIme.Size = new System.Drawing.Size(24, 13);
+            this.lblIme.Size = new System.Drawing.Size(29, 16);
             this.lblIme.TabIndex = 1;
             this.lblIme.Text = "Ime";
             // 
             // lblVrsta
             // 
             this.lblVrsta.AutoSize = true;
-            this.lblVrsta.Location = new System.Drawing.Point(12, 52);
+            this.lblVrsta.Location = new System.Drawing.Point(16, 64);
+            this.lblVrsta.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblVrsta.Name = "lblVrsta";
-            this.lblVrsta.Size = new System.Drawing.Size(31, 13);
+            this.lblVrsta.Size = new System.Drawing.Size(38, 16);
             this.lblVrsta.TabIndex = 2;
             this.lblVrsta.Text = "Vrsta";
             // 
             // lblPasmina
             // 
             this.lblPasmina.AutoSize = true;
-            this.lblPasmina.Location = new System.Drawing.Point(12, 86);
+            this.lblPasmina.Location = new System.Drawing.Point(16, 106);
+            this.lblPasmina.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPasmina.Name = "lblPasmina";
-            this.lblPasmina.Size = new System.Drawing.Size(47, 13);
+            this.lblPasmina.Size = new System.Drawing.Size(60, 16);
             this.lblPasmina.TabIndex = 3;
             this.lblPasmina.Text = "Pasmina";
             // 
             // txtVrsta
             // 
-            this.txtVrsta.Location = new System.Drawing.Point(66, 45);
+            this.txtVrsta.Location = new System.Drawing.Point(88, 55);
+            this.txtVrsta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtVrsta.Name = "txtVrsta";
-            this.txtVrsta.Size = new System.Drawing.Size(149, 20);
+            this.txtVrsta.Size = new System.Drawing.Size(197, 22);
             this.txtVrsta.TabIndex = 4;
             // 
             // txtPasmina
             // 
-            this.txtPasmina.Location = new System.Drawing.Point(66, 79);
+            this.txtPasmina.Location = new System.Drawing.Point(88, 97);
+            this.txtPasmina.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPasmina.Name = "txtPasmina";
-            this.txtPasmina.Size = new System.Drawing.Size(149, 20);
+            this.txtPasmina.Size = new System.Drawing.Size(197, 22);
             this.txtPasmina.TabIndex = 5;
             // 
             // lblSpol
             // 
             this.lblSpol.AutoSize = true;
-            this.lblSpol.Location = new System.Drawing.Point(12, 119);
+            this.lblSpol.Location = new System.Drawing.Point(16, 146);
+            this.lblSpol.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSpol.Name = "lblSpol";
-            this.lblSpol.Size = new System.Drawing.Size(28, 13);
+            this.lblSpol.Size = new System.Drawing.Size(35, 16);
             this.lblSpol.TabIndex = 6;
             this.lblSpol.Text = "Spol";
             // 
             // rbM
             // 
             this.rbM.AutoSize = true;
-            this.rbM.Location = new System.Drawing.Point(66, 117);
+            this.rbM.Location = new System.Drawing.Point(88, 144);
+            this.rbM.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rbM.Name = "rbM";
-            this.rbM.Size = new System.Drawing.Size(34, 17);
+            this.rbM.Size = new System.Drawing.Size(39, 20);
             this.rbM.TabIndex = 7;
             this.rbM.TabStop = true;
             this.rbM.Text = "M";
@@ -125,9 +138,10 @@
             // rbZ
             // 
             this.rbZ.AutoSize = true;
-            this.rbZ.Location = new System.Drawing.Point(106, 117);
+            this.rbZ.Location = new System.Drawing.Point(141, 144);
+            this.rbZ.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rbZ.Name = "rbZ";
-            this.rbZ.Size = new System.Drawing.Size(32, 17);
+            this.rbZ.Size = new System.Drawing.Size(36, 20);
             this.rbZ.TabIndex = 8;
             this.rbZ.TabStop = true;
             this.rbZ.Text = "Ž";
@@ -135,43 +149,48 @@
             // 
             // nudDob
             // 
-            this.nudDob.Location = new System.Drawing.Point(66, 140);
+            this.nudDob.Location = new System.Drawing.Point(88, 172);
+            this.nudDob.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.nudDob.Name = "nudDob";
-            this.nudDob.Size = new System.Drawing.Size(149, 20);
+            this.nudDob.Size = new System.Drawing.Size(199, 22);
             this.nudDob.TabIndex = 9;
             // 
             // lblDob
             // 
             this.lblDob.AutoSize = true;
-            this.lblDob.Location = new System.Drawing.Point(12, 142);
+            this.lblDob.Location = new System.Drawing.Point(16, 175);
+            this.lblDob.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDob.Name = "lblDob";
-            this.lblDob.Size = new System.Drawing.Size(27, 13);
+            this.lblDob.Size = new System.Drawing.Size(33, 16);
             this.lblDob.TabIndex = 10;
             this.lblDob.Text = "Dob";
             // 
             // lblDatumDolaska
             // 
             this.lblDatumDolaska.AutoSize = true;
-            this.lblDatumDolaska.Location = new System.Drawing.Point(12, 171);
+            this.lblDatumDolaska.Location = new System.Drawing.Point(16, 210);
+            this.lblDatumDolaska.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDatumDolaska.Name = "lblDatumDolaska";
-            this.lblDatumDolaska.Size = new System.Drawing.Size(78, 13);
+            this.lblDatumDolaska.Size = new System.Drawing.Size(98, 16);
             this.lblDatumDolaska.TabIndex = 11;
             this.lblDatumDolaska.Text = "Datum dolaska";
             this.lblDatumDolaska.Click += new System.EventHandler(this.label6_Click);
             // 
             // dtpDatumDolaska
             // 
-            this.dtpDatumDolaska.Location = new System.Drawing.Point(15, 187);
+            this.dtpDatumDolaska.Location = new System.Drawing.Point(20, 230);
+            this.dtpDatumDolaska.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dtpDatumDolaska.Name = "dtpDatumDolaska";
-            this.dtpDatumDolaska.Size = new System.Drawing.Size(200, 20);
+            this.dtpDatumDolaska.Size = new System.Drawing.Size(265, 22);
             this.dtpDatumDolaska.TabIndex = 12;
             // 
             // cbCijepljen
             // 
             this.cbCijepljen.AutoSize = true;
-            this.cbCijepljen.Location = new System.Drawing.Point(15, 217);
+            this.cbCijepljen.Location = new System.Drawing.Point(20, 267);
+            this.cbCijepljen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbCijepljen.Name = "cbCijepljen";
-            this.cbCijepljen.Size = new System.Drawing.Size(65, 17);
+            this.cbCijepljen.Size = new System.Drawing.Size(81, 20);
             this.cbCijepljen.TabIndex = 13;
             this.cbCijepljen.Text = "Cijepljen";
             this.cbCijepljen.UseVisualStyleBackColor = true;
@@ -179,9 +198,10 @@
             // cbKastriran
             // 
             this.cbKastriran.AutoSize = true;
-            this.cbKastriran.Location = new System.Drawing.Point(99, 217);
+            this.cbKastriran.Location = new System.Drawing.Point(132, 267);
+            this.cbKastriran.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbKastriran.Name = "cbKastriran";
-            this.cbKastriran.Size = new System.Drawing.Size(67, 17);
+            this.cbKastriran.Size = new System.Drawing.Size(81, 20);
             this.cbKastriran.TabIndex = 14;
             this.cbKastriran.Text = "Kastriran";
             this.cbKastriran.UseVisualStyleBackColor = true;
@@ -189,56 +209,114 @@
             // lblNapomena
             // 
             this.lblNapomena.AutoSize = true;
-            this.lblNapomena.Location = new System.Drawing.Point(12, 246);
+            this.lblNapomena.Location = new System.Drawing.Point(16, 303);
+            this.lblNapomena.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNapomena.Name = "lblNapomena";
-            this.lblNapomena.Size = new System.Drawing.Size(59, 13);
+            this.lblNapomena.Size = new System.Drawing.Size(75, 16);
             this.lblNapomena.TabIndex = 15;
             this.lblNapomena.Text = "Napomena";
             // 
             // txtNapomena
             // 
-            this.txtNapomena.Location = new System.Drawing.Point(15, 262);
+            this.txtNapomena.Location = new System.Drawing.Point(20, 322);
+            this.txtNapomena.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNapomena.Name = "txtNapomena";
-            this.txtNapomena.Size = new System.Drawing.Size(200, 20);
+            this.txtNapomena.Size = new System.Drawing.Size(265, 22);
             this.txtNapomena.TabIndex = 16;
             // 
-            // openFileDialog1
+            // OpenFileDialog
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
+            this.OpenFileDialog.FileName = "openFileDialog1";
             // 
             // lblOdabir
             // 
             this.lblOdabir.AutoSize = true;
-            this.lblOdabir.Location = new System.Drawing.Point(12, 295);
+            this.lblOdabir.Location = new System.Drawing.Point(16, 363);
+            this.lblOdabir.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblOdabir.Name = "lblOdabir";
-            this.lblOdabir.Size = new System.Drawing.Size(139, 13);
+            this.lblOdabir.Size = new System.Drawing.Size(175, 16);
             this.lblOdabir.TabIndex = 17;
             this.lblOdabir.Text = "Odabir i prikaz slike životinje";
             // 
             // pbOdabir
             // 
-            this.pbOdabir.Location = new System.Drawing.Point(15, 311);
+            this.pbOdabir.Location = new System.Drawing.Point(20, 383);
+            this.pbOdabir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pbOdabir.Name = "pbOdabir";
-            this.pbOdabir.Size = new System.Drawing.Size(120, 87);
+            this.pbOdabir.Size = new System.Drawing.Size(160, 107);
             this.pbOdabir.TabIndex = 18;
             this.pbOdabir.TabStop = false;
             // 
-            // button1
+            // btnSpremi
             // 
-            this.button1.Location = new System.Drawing.Point(15, 404);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 19;
-            this.button1.Text = "Spremi";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnSpremi.Location = new System.Drawing.Point(20, 497);
+            this.btnSpremi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(100, 28);
+            this.btnSpremi.TabIndex = 19;
+            this.btnSpremi.Text = "Spremi";
+            this.btnSpremi.UseVisualStyleBackColor = true;
+            this.btnSpremi.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnOdaberiSliku
+            // 
+            this.btnOdaberiSliku.Location = new System.Drawing.Point(198, 383);
+            this.btnOdaberiSliku.Name = "btnOdaberiSliku";
+            this.btnOdaberiSliku.Size = new System.Drawing.Size(109, 23);
+            this.btnOdaberiSliku.TabIndex = 20;
+            this.btnOdaberiSliku.Text = "Odaberi sliku";
+            this.btnOdaberiSliku.UseVisualStyleBackColor = true;
+            // 
+            // cbStatus
+            // 
+            this.cbStatus.FormattingEnabled = true;
+            this.cbStatus.Items.AddRange(new object[] {
+            "U azilu",
+            "Udomljen/a"});
+            this.cbStatus.Location = new System.Drawing.Point(329, 170);
+            this.cbStatus.Name = "cbStatus";
+            this.cbStatus.Size = new System.Drawing.Size(121, 24);
+            this.cbStatus.TabIndex = 21;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(326, 146);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(44, 16);
+            this.lblStatus.TabIndex = 22;
+            this.lblStatus.Text = "Status";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(326, 210);
+            this.lblID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(20, 16);
+            this.lblID.TabIndex = 23;
+            this.lblID.Text = "ID";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(329, 230);
+            this.txtID.Margin = new System.Windows.Forms.Padding(4);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(197, 22);
+            this.txtID.TabIndex = 24;
             // 
             // frmUnosZivotinje
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.cbStatus);
+            this.Controls.Add(this.btnOdaberiSliku);
+            this.Controls.Add(this.btnSpremi);
             this.Controls.Add(this.pbOdabir);
             this.Controls.Add(this.lblOdabir);
             this.Controls.Add(this.txtNapomena);
@@ -258,6 +336,7 @@
             this.Controls.Add(this.lblVrsta);
             this.Controls.Add(this.lblIme);
             this.Controls.Add(this.txtIme);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmUnosZivotinje";
             this.Text = "frmUnosZivotinje";
             ((System.ComponentModel.ISupportInitialize)(this.nudDob)).EndInit();
@@ -286,9 +365,14 @@
         private System.Windows.Forms.CheckBox cbKastriran;
         private System.Windows.Forms.Label lblNapomena;
         private System.Windows.Forms.TextBox txtNapomena;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
         private System.Windows.Forms.Label lblOdabir;
         private System.Windows.Forms.PictureBox pbOdabir;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSpremi;
+        private System.Windows.Forms.Button btnOdaberiSliku;
+        private System.Windows.Forms.ComboBox cbStatus;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtID;
     }
 }
