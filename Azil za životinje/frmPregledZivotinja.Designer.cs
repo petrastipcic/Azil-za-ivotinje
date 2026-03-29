f2aaf71c6d2a5debae3681a6c09074c08fe25219
petrastipcic <petra.stipcic@skole.hr>
2026-03-29 17:34:24 +0200

    Promjene

---
+++ Azil za životinje/frmPregledZivotinja.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Azil_za_životinje
+{
+    partial class frmPregledZivotinja
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbZivotinje = new System.Windows.Forms.ListBox();
+            this.pbSlika = new System.Windows.Forms.PictureBox();
+            this.lblDetalji = new System.Windows.Forms.Label();
+            this.lblSlika = new System.Windows.Forms.Label();
+            this.lblListaZivotinja = new System.Windows.Forms.Label();
+            this.txtDetalji = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSlika)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbZivotinje
+            // 
+            this.lbZivotinje.FormattingEnabled = true;
+            this.lbZivotinje.ItemHeight = 16;
+            this.lbZivotinje.Location = new System.Drawing.Point(12, 28);
+            this.lbZivotinje.Name = "lbZivotinje";
+            this.lbZivotinje.Size = new System.Drawing.Size(401, 500);
+            this.lbZivotinje.TabIndex = 0;
+            this.lbZivotinje.SelectedIndexChanged += new System.EventHandler(this.lbZivotinje_SelectedIndexChanged);
+            // 
+            // pbSlika
+            // 
+            this.pbSlika.Location = new System.Drawing.Point(419, 28);
+            this.pbSlika.Name = "pbSlika";
+            this.pbSlika.Size = new System.Drawing.Size(353, 495);
+            this.pbSlika.TabIndex = 1;
+            this.pbSlika.TabStop = false;
+            // 
+            // lblDetalji
+            // 
+            this.lblDetalji.AutoSize = true;
+            this.lblDetalji.Location = new System.Drawing.Point(775, 9);
+            this.lblDetalji.Name = "lblDetalji";
+            this.lblDetalji.Size = new System.Drawing.Size(45, 16);
+            this.lblDetalji.TabIndex = 2;
+            this.lblDetalji.Text = "Detalji";
+            // 
+            // lblSlika
+            // 
+            this.lblSlika.AutoSize = true;
+            this.lblSlika.Location = new System.Drawing.Point(416, 9);
+            this.lblSlika.Name = "lblSlika";
+            this.lblSlika.Size = new System.Drawing.Size(37, 16);
+            this.lblSlika.TabIndex = 3;
+            this.lblSlika.Text = "Slika";
+            // 
+            // lblListaZivotinja
+            // 
+            this.lblListaZivotinja.AutoSize = true;
+            this.lblListaZivotinja.Location = new System.Drawing.Point(12, 9);
+            this.lblListaZivotinja.Name = "lblListaZivotinja";
+            this.lblListaZivotinja.Size = new System.Drawing.Size(85, 16);
+            this.lblListaZivotinja.TabIndex = 4;
+            this.lblListaZivotinja.Text = "ListaZivotinja";
+            // 
+            // txtDetalji
+            // 
+            this.txtDetalji.Location = new System.Drawing.Point(778, 28);
+            this.txtDetalji.Multiline = true;
+            this.txtDetalji.Name = "txtDetalji";
+            this.txtDetalji.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDetalji.Size = new System.Drawing.Size(421, 495);
+            this.txtDetalji.TabIndex = 5;
+            // 
+            // frmPregledZivotinja
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1211, 535);
+            this.Controls.Add(this.txtDetalji);
+            this.Controls.Add(this.lblListaZivotinja);
+            this.Controls.Add(this.lblSlika);
+            this.Controls.Add(this.lblDetalji);
+            this.Controls.Add(this.pbSlika);
+            this.Controls.Add(this.lbZivotinje);
+            this.Name = "frmPregledZivotinja";
+            this.Text = "frmPregledZivotinja";
+            this.Load += new System.EventHandler(this.frmPregledZivotinja_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSlika)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbZivotinje;
+        private System.Windows.Forms.PictureBox pbSlika;
+        private System.Windows.Forms.Label lblDetalji;
+        private System.Windows.Forms.Label lblSlika;
+        private System.Windows.Forms.Label lblListaZivotinja;
+        private System.Windows.Forms.TextBox txtDetalji;
+    }
+}
---
+++ Azil za životinje/frmPregledZivotinja.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Azil_za_životinje
+{
+    public partial class frmPregledZivotinja : Form
+    {
+        public frmPregledZivotinja()
+        {
+            InitializeComponent();
+        }
+        List<Zivotinja> lista;
+
+        private void frmPregledZivotinja_Load(object sender, EventArgs e)
+        {
+            lista = Datoteka.UcitajSve();
+            lbZivotinje.Items.Clear();
+            foreach (var z in lista)
+            {
+                lbZivotinje.Items.Add(z.Ime + " (" + z.Vrsta + ")");
+
+            }
+        }
+
+        private void lbZivotinje_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbZivotinje.SelectedIndex == -1) return;
+
+            Zivotinja z = lista[lbZivotinje.SelectedIndex];
+
+            if (File.Exists(z.PutanjaSlike))
+                pbSlika.ImageLocation = z.PutanjaSlike;
+            else
+                pbSlika.Image = null;
+
+
+
+                txtDetalji.Text =
+                    "ID: " + z.ID + Environment.NewLine +
+                    "Ime: " + z.Ime + Environment.NewLine +
+                    "Vrsta: " + z.Vrsta + Environment.NewLine +
+                    "Pasmina: " + z.Pasmina + Environment.NewLine +
+                    "Spol: " + z.Spol + Environment.NewLine +
+                    "Dob: " + z.Dob + Environment.NewLine +
+                    "Status: " + z.Status + Environment.NewLine +
+                    "Datum dolaska: " + z.DatumDolaska.ToShortDateString() + Environment.NewLine +
+                    "Datum udomljavanja: " + (z.DatumUdomljavanja == DateTime.MinValue ? "Nije udomljen" : z.DatumUdomljavanja.ToShortDateString()) + Environment.NewLine +
+                    "Cijepljen: " + (z.Cijepljen ? "Da" : "Ne") + Environment.NewLine +
+                    "Kastriran: " + (z.Kastriran ? "Da" : "Ne") + Environment.NewLine +
+                    "Napomena: " + z.Napomena + Environment.NewLine +
+                    "Udomitelj: " + z.Udomitelj + Environment.NewLine +
+                    "Kontakt: " + z.Kontakt;
+
+        }
+    }
+}
